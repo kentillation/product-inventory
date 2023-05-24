bb87f1934ddf7b8a79f2214566984f4e19aa5fa0
Kert Tatoy <tatoy.kert.e@gmail.com>
2023-05-24 15:41:37 +0800

    Finished product module

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ProductModel;
+use App\Models\CategoryModel;
+use App\Models\UnitModel;
+
+class ProductController extends Controller
+{
+    //
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $products = ProductModel::select('tbl_products.*, tbl_category.category', 'tbl_units.unit')
+            ->join('tbl_category', 'tbl_products.category_id', '=', 'tbl_category.id')
+            ->join('tbl_units', 'tbl_products.unit_id', '=', 'tbl_units.id')
+            ->where('tbl_products.name', 'like', '%' . $search . '%')
+            ->get();
+
+        $categories = CategoryModel::all();
+        $units = UnitModel::all();
+        return view('products/index', ['products' => $products, 'categories' => $categories, 'units' => $units]);
+    }
+
+    public function get($id)
+    {
+        $product = ProductModel::with(['tbl_category', 'tbl_units'])->findorfail($id);
+        return view('products/edit-product', ['product' => $product]);
+    }
+
+    public function save(Request $request)
+    {
+        try {
+            $data = [
+                'name' => $request->input('name'),
+                'price' => $request->input('price'),
+                'description' => $request->input('description'),
+                'unit_id' => $request->input('unit_id'),
+                'availability' => $request->input('availability'),
+                'quantity' => $request->input('quantity'),
+                'category_id' => $request->input('category_id'),
+            ];
+
+            ProductModel::create($data);
+            return redirect()->route('search-product')->with('success', 'Product added successfully');
+        } catch (\Exception $e) {
+            return redirect()->route('search-product')->with('error', 'Failed to add the product');
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $product = ProductModel::findorfail($id);
+            $data = [
+                'name' => $request->input('name'),
+                'price' => $request->input('price'),
+                'description' => $request->input('description'),
+                'unit_id' => $request->input('unit_id'),
+                'availability' => $request->input('availability'),
+                'quantity' => $request->input('quantity'),
+                'category_id' => $request->input('category_id'),
+            ];
+
+            $product->update($data);
+            return redirect()->route('search-product')->with('success', 'Product update successfully');
+        } catch (\Exception $e) {
+            return redirect()->route('search-product')->with('error', 'Failed to update the product');
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $product = ProductModel::find($id);
+            $product->delete();
+            return redirect()->route('search-product')->with('success', 'Product deleted successfully');
+        } catch (\Exception $e) {
+            return redirect()->route('search-product')->with('error', 'Failed to delete the product');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
     return view('index');
 });
+
+// PRODUCT
+Route::get('/products', 'ProductController@search')->name('search-product');
+Route::get('/products/edit/{id}', 'ProductController@get')->name('get-product');
+Route::post('/products/save', 'ProductController@save')->name('save-product');
+Route::put('/products/edit/{id}', 'ProductController@update')->name('update-product');
+Route::delete('/products/delete/{id}', 'ProductController@save')->name('delete-product');
